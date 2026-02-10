09edd00d82db4f997f107807fdd54d4d50ddc9c0
Ivan <ivantieplih@gmail.com>
2026-02-10 22:16:09 +0200

    04

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,7 +2,7 @@
 import 'izitoast/dist/css/iziToast.min.css';
 import iziToast from 'izitoast';
 
-const form = document.querySelector('.snackbar-form');
+const form = document.querySelector('.form');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -21,27 +21,31 @@ form.addEventListener('submit', event => {
   }
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-      state === 'fulfilled' ? resolve() : reject();
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
     }, delay);
   });
+
   promise
-    .then(() => {
+    .then(delayValue => {
       iziToast.success({
         position: 'topRight',
         backgroundColor: '#5cb85c',
         messageColor: '#fff',
-        message: `Fulfilled promise in ${delay}ms`,
+        message: `Fulfilled promise in ${delayValue}ms`,
         close: false,
       });
     })
-    .catch(() => {
+    .catch(delayValue => {
       iziToast.error({
         position: 'topRight',
         backgroundColor: '#bd221dff',
         messageColor: '#fff',
-        message: `Rejected promise in ${delay}ms`,
+        message: `Rejected promise in ${delayValue}ms`,
         close: false,
       });
     });
-  form.reset();
 });
